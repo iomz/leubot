704554473a237b8b4a53fa998ea9a83f125d4593
Iori Mizutani <iori.mizutani@gmail.com>
2019-12-24 21:26:05 +0900

    Add docstring for api_token.go

--- api/token.go
+++ api/token.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 )
 
+// Token for the user token
 type Token struct {
 	Token string `json:"token"`
 }
 
+// GenerateToken creates a new token
 func GenerateToken() string {
 	b := make([]byte, 16) // 16-byte token
 	rand.Read(b)
